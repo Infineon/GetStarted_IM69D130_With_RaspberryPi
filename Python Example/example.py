df4126317a2a5f6aca077c26b88226ab75b54ba1
Swarnam Panday <pandayswarnam@gmail.com>
2019-11-04 13:04:08 +0530

    Adding python example

---
+++ Python Example/example.py
@@ -0,0 +1,35 @@
+import pyaudio
+import wave
+
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 44100
+CHUNK = 512
+RECORD_SECONDS = 5
+WAVE_OUTPUT_FILENAME = "recordedFile.wav"
+device_index = 2
+audio = pyaudio.PyAudio()
+
+
+
+stream = audio.open(format=FORMAT, channels=CHANNELS,
+                rate=RATE, input=True,input_device_index = 5,
+                frames_per_buffer=CHUNK)
+print ("recording started")
+Recordframes = []
+
+for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    data = stream.read(CHUNK)
+    Recordframes.append(data)
+print ("recording stopped")
+
+stream.stop_stream()
+stream.close()
+audio.terminate()
+
+waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+waveFile.setnchannels(CHANNELS)
+waveFile.setsampwidth(audio.get_sample_size(FORMAT))
+waveFile.setframerate(RATE)
+waveFile.writeframes(b''.join(Recordframes))
+waveFile.close()
